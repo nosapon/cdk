de29edd9eb32f2fb56b0a742c4154780c0cd7afa
nosapon <black1robin1@moegi.waseda.jp>
2023-05-30 16:07:30 +0900

    cdk-workshop.tsにコメントアウトを入れた

--- cdk-workshop/bin/cdk-workshop.ts
+++ cdk-workshop/bin/cdk-workshop.ts
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
+// このスクリプトをNode.js環境で実行するための設定
+
 import * as cdk from 'aws-cdk-lib';
+// AWS CDKのライブラリをインポート。これによりAWSの各種リソースをプログラムで操作できるようになる
+
 import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
+// '../lib/cdk-workshop-stack' ファイルからCdkWorkshopStackクラスをインポート
 
 const app = new cdk.App();
+// CDKアプリケーションのインスタンスを生成。これはAWSのリソースをデプロイするための基盤となる
+
 new CdkWorkshopStack(app, 'CdkWorkshopStack');
+// CdkWorkshopStackクラスの新しいインスタンスを生成し、appというCDKアプリケーションを親として関連づける。
+// 'CdkWorkshopStack' はこのスタックの一意な識別子
